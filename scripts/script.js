14854aacab376b92df1fe839a13d447a583dceaf
timmit147 <timmeeuwsen@hotmail.nl>
2022-10-22 18:45:42 +0200

    include working

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
-function include(block){
-    fetch(`blocks/sidebar/`+block+`.html`)
-        .then(response=> response.text())
-        .then(text=>  document.body.innerHTML = text);
+const block = document.querySelector('.include').innerHTML;
 
-    var file = location.pathname.split( "blocks/sidebar/sidebar.css" ).pop();
-    console.log("test");
-    
-    var link = document.createElement( "link" );
-    link.href = file.substr( 0, file.lastIndexOf( "." ) ) + "blocks/sidebar/sidebar.css";
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    link.media = "screen,print";
-    
-    document.getElementsByTagName( "head" )[0].appendChild( link );
-}
+console.log(block);
+
+fetch(`blocks/sidebar/`+block+`.html`)
+    .then(response=> response.text())
+    .then(text=>  document.body.innerHTML = text);
+
+var file = location.pathname.split( `blocks/sidebar/`+block+`.css` ).pop();
+var link = document.createElement( "link" );
+link.href = file.substr( 0, file.lastIndexOf( "." ) ) + "blocks/sidebar/sidebar.css";
+link.type = "text/css";
+link.rel = "stylesheet";
+link.media = "screen,print";
+document.getElementsByTagName( "head" )[0].appendChild( link );
+
 
 
