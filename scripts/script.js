80b3e2987ce8dcfbcc1f99504023bc9c09b3c1f3
timmit147 <timmeeuwsen@hotmail.nl>
2022-10-22 21:27:20 +0200

    blocks working

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,30 @@
-const block = document.querySelector('.include').innerHTML;
+const blocks = document.querySelectorAll(".include");
 
-console.log(block);
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+      return div.firstChild;
+  }
 
-fetch(`blocks/sidebar/`+block+`.html`)
+blocks.forEach((block) => {
+    blockName = block.innerHTML;
+    block = block;
+    fetch(`blocks/`+blockName+`/block.html`)
     .then(response=> response.text())
-    .then(text=>  document.body.innerHTML = text);
+    .then(text=> block.parentNode.replaceChild(createElementFromHTML(text), block)
+    );
 
-var file = location.pathname.split( `blocks/sidebar/`+block+`.css` ).pop();
-var link = document.createElement( "link" );
-link.href = file.substr( 0, file.lastIndexOf( "." ) ) + "blocks/sidebar/sidebar.css";
-link.type = "text/css";
-link.rel = "stylesheet";
-link.media = "screen,print";
-document.getElementsByTagName( "head" )[0].appendChild( link );
+
+
+    var file = location.pathname.split( `blocks/`+blockName+`/style.css` ).pop();
+    var link = document.createElement( "link" );
+    link.href = file.substr( 0, file.lastIndexOf( "." ) ) + `blocks/`+blockName+`/style.css`;
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.media = "screen,print";
+    document.getElementsByTagName( "head" )[0].appendChild( link );
+});
+
 
 
 
