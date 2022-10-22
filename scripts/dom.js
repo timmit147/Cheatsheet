0716c627249c00654248a30f293e4edc61cbd12f
timmit147 <timmeeuwsen@hotmail.nl>
2022-10-22 18:15:00 +0200

    fetch works but not css

---
+++ scripts/dom.js
@@ -0,0 +1,13 @@
+// var css = 'h1 { background: red; }',
+//     head = document.head || document.getElementsByTagName('head')[0],
+//     style = document.createElement('style');
+
+// head.appendChild(style);
+
+// style.type = 'text/css';
+// if (style.styleSheet){
+//   // This is required for IE8 and below.
+//   style.styleSheet.cssText = css;
+// } else {
+//   style.appendChild(document.createTextNode(css));
+// }
